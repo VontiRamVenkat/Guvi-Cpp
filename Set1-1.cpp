31e4b78e4dacca16ac72110152616d171ed98364
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-02-17 16:14:13 +0530

    Create Set1-1.cpp

---
+++ Set1-1.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    float num;
+    cout<<"Please enter number"<<endl;
+    cin>>num;
+
+    if(num>0)
+    cout<<"number is positive"<<endl;
+
+   if(num<0)
+    cout<<"number is negative"<<endl;
+
+   if(num==0)
+    cout<<"number is zero"<<endl;
+    system ("pause");
+
+    return 0;
+}
