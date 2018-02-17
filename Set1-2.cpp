67ee07c724d1d9c55f99ef702c772c139d8cdec8
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-02-17 16:21:33 +0530

    Create Set1-2.cpp

---
+++ Set1-2.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+    int num;
+    cout<<"Please enter number";
+    cin>>num;
+
+    if(num%2==0)
+    cout<<"number is Even";
+
+    else
+    cout<<"number is Odd";
+
+    return 0;
+}
