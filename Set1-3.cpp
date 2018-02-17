9002b4446951560fdbf76de27fd631620677bb4f
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-02-17 16:29:46 +0530

    Create Set1-3.cpp

---
+++ Set1-3.cpp
@@ -0,0 +1,58 @@
+#include <iostream>
+using namespace std;
+
+
+int main()
+{
+   char ch;
+      cout<<"Enter any character from  A to Z := ";
+      cin>>ch;
+      switch(ch)
+      {
+      case 'A':
+           cout<<"Vowel";
+           break;
+          
+      case 'E':
+           cout<<"Vowel";
+           break;
+     
+      case 'I':
+           cout<<"Vowel";
+           break;
+     
+      case 'O':
+           cout<<"Vowel";
+           break;
+     
+      case 'U':
+           cout<<"Vowel";
+           break;
+                
+      case 'a':
+           cout<<"Vowel";
+           break;
+          
+      case 'e':
+           cout<<"Vowel";
+           break;
+     
+      case 'i':
+           cout<<"Vowel";
+           break;
+     
+      case 'o':
+           cout<<"Vowel";
+           break;
+     
+      case 'u':
+           cout<<"Vowel";
+           break;                                        
+            
+      default:
+           cout<<"consonant";
+     
+    return 0;
+
+}
+}
