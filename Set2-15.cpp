74cfcbbcdb9a256320d01b8c6ff62eff414fc558
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-02-28 14:00:15 +0530

    Update Set2-15.cpp

--- Set2-15.cpp
+++ Set2-15.cpp
@@ -6,10 +6,10 @@ int main()
     cout<<"Print Even Numbers in a given range m to n:\n";
     cin>>m;
     cin>>n;
-for (num = m; num <= n; num++)
-        {
-               if (num % 2 == 0)
+for (num = m; num < n; num++)
+{
+               if (num % 2 == 0) 
                   cout<<num;
-         }
+         } 
                 return 0;
 }
