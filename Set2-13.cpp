2a8624e0ec955a612e8622646ba56c3f7b971945
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-02-28 10:37:38 +0530

    Update Set2-13.cpp

--- Set2-13.cpp
+++ Set2-13.cpp
@@ -1,26 +1,26 @@
-#include <iostream>
+#include<iostream>
 using namespace std;
-
 int main()
 {
-  int n, i;
-  bool isPrime = true;
 
-  cout << "Enter a positive integer: ";
-  cin >> n;
-
-  for(i = 2; i <= n / 2; ++i)
-  {
-      if(n % i == 0)
-      {
-          isPrime = false;
-          break;
-      }
-  }
-  if (isPrime)
-      cout << "This is a prime number";
-  else
-      cout << "This is not a prime number";
-
-  return 0;
+	int num,i,count=0;
+	cout<<"Enter a number:";
+	cin>>num;
+	for(i=2;i<num;i++)
+	{
+		if(num%i==0)
+		{
+			count++;
+			break;
+		}
+	}
+	if(count==0)
+	{
+		cout<<"This is a prime number";
+	}
+	else
+	{
+		cout<<"This is not a prime number";
+	}
+	return 0;
 }
