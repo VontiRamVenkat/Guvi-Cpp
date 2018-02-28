c2d9d62cfc0b247a8bfe6449145ceec72eaa35e0
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-02-28 13:58:52 +0530

    Create Set2-15.cpp

---
+++ Set2-15.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int num,m,n;
+    cout<<"Print Even Numbers in a given range m to n:\n";
+    cin>>m;
+    cin>>n;
+for (num = m; num <= n; num++)
+        {
+               if (num % 2 == 0)
+                  cout<<num;
+         }
+                return 0;
+}
