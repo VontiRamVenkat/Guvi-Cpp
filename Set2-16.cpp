5289ab489c0e1076c51caa5c9e1069b68f8b40a9
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-02-28 14:05:54 +0530

    Create Set2-16.cpp

---
+++ Set2-16.cpp
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a, b, c, i;
+    cout << "Enter two numbers(intervals): ";
+    cin >> a >> b;
+    cout << "Prime numbers between " << a << " and " << b << " are: ";
+    while (a<b)
+    {
+        c = 0;
+        for(i = 2; i <= a/2; ++i)
+        {
+            if(a% i == 0)
+            {
+                c= 1;
+                break;
+            }
+        }
+        if (c== 0)
+            cout <<a<< " ";
+        ++a;
+    }
+    return 0;
+}
