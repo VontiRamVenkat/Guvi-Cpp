c78a54719b0a521f1665549efc2ce0837bc44f1a
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-02 14:53:14 +0530

    Create Set3-23.cpp

---
+++ Set3-23.cpp
@@ -0,0 +1,21 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int Arr[100],n,i,small;
+	cout<<"Enter number of elements you want to insert ";
+	cin>>n;
+	for(i=0;i<n;i++)
+	{
+		cout<<"Enter element "<<i+1<<":";
+		cin>>Arr[i];
+	}
+	small=Arr[0];
+	for(i=1;i<n;i++)
+	{
+		if(Arr[i]<small)
+			small=Arr[i];
+	}
+	cout<<"\nSmallest element is :"<<small;
+	return 0;
+}
