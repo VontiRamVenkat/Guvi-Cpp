046da91c298acdfe55e28375cc2d0edd26bf4821
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-13 22:42:22 +0530

    Update set6-53.cpp

--- set6-53.cpp
+++ set6-53.cpp
@@ -5,7 +5,8 @@ int main()
     int num,sum = 0,rem;
     cout<<"Enter a number"<<endl;
     cin>>num;
-    while(num != 0){\
+    while(num != 0)
+    {
         rem = num%10;
         sum += rem;
         num /= 10;
