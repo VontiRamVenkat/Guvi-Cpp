8f1e5f6cbb0a8c474f7eb083046beb7a877c22e3
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-13 22:24:20 +0530

    Update set5-42.cpp

--- set5-42.cpp
+++ set5-42.cpp
@@ -5,11 +5,15 @@ int main()
     string str1,str2;
     cout<<"Enter the two strings"<<endl;
     cin>>str1>>str2;
-    if(str1.length() == str2.length()){
+    if(str1.length() == str2.length())
+    {
         cout<<str2;
-    }else if(str1.length() > str2.length()){
+    }
+    else if(str1.length() > str2.length())
+    {
         cout<<"Greater string is:"<<str1;
-    }else{
+    }else
+    {
         cout<<"Greater string is:"<<str2;
     }
     return 0;
