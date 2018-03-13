660c53e496472feac449f7b09c294d0cb0ad9926
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-13 22:37:24 +0530

    Update set5-50.cpp

--- set5-50.cpp
+++ set5-50.cpp
@@ -1,24 +1,28 @@
 #include <iostream>
 #include <math.h>
-
 using namespace std;
-
 int main()
 {
     int i,result,set = 0;
     cout<<"Enter a number :";
     cin>>i;
-    for(int j = 0;j <= i;j++){
+    for(int j = 0;j <= i;j++)
+    {
         result = pow(2,j);
-        if(result == i){
+        if(result == i)
+        {
             set = 1;
-        }else{
+        }
+        else
+        {
             set = 0;
         }
     }
-    if(set == 1){
+    if(set == 1)
+    {
         cout<<"Its a power of 2";
-    }else{
+    }else
+    {
         cout<<"Its not a power of 2";
     }
     return 0;
