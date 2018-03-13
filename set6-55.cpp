f73a4f1f08efc7f79f21ad074d13893ff52896b8
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-13 22:47:32 +0530

    Update set6-55.cpp

--- set6-55.cpp
+++ set6-55.cpp
@@ -1,16 +1,17 @@
 #include <iostream>
-
 using namespace std;
-
 int main()
 {
     int a,b;
     cout<<"Enter the numbers :"<<endl;
     cin>>a>>b;
     a = a * b;
-    if(a % 2 == 0){
+    if(a % 2 == 0)
+    {
         cout<<"Its a even number";
-    }else{
+    }
+    else
+    {
         cout<<"Its a odd number";
     }
 
