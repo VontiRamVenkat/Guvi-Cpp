2c07e476a3dbf3e229509342baf554c27591beac
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-13 22:34:15 +0530

    Update set5-47.cpp

--- set5-47.cpp
+++ set5-47.cpp
@@ -1,24 +1,30 @@
 #include <iostream>
 using namespace std;
-
 int main()
 {
     int n,a[n],max,min;
     cout<<"Enter a number:";
     cin>>n;
-    for(int j = 0;j < n;j++){
+    for(int j = 0;j < n;j++)
+    {
         cin>>a[j];
     }
-    if(n == 1){
+    if(n == 1)
+    {
         cout<<"Minimum of the array:"<<a[0]<<endl;
         cout<<"Maximum of the array:"<<a[0]<<endl;
-    }else{
+    }
+    else
+    {
         min = a[0];
-        for(int j = 1;j < n;j++){
-            if(a[j] > min){
+        for(int j = 1;j < n;j++)
+        {
+            if(a[j] > min)
+            {
                 max = a[j];
             }
-            if(a[j] < min){
+            if(a[j] < min)
+            {
                 min = a[j];
             }
         }
