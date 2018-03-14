4a877ca21ca12542cb4e5647e4ebac1f9c695010
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-14 22:12:55 +0530

    Create Set6-59.cpp

---
+++ Set6-59.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,i,temp;
+    cout<<"Enter any 10 numbers:";
+    for (i=1;i<=10;i++)
+    {
+    cin>>a;
+    if(temp<n)
+    {
+        temp=n;
+    }
+    }
+    cout<<temp;
+    return 0;
+}
