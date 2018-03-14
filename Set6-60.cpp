ab5576273cb2567cb3d8ffbd679ae9f233307e8a
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-14 22:16:36 +0530

    Create Set6-60.cpp

---
+++ Set6-60.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,j,f1,f2,f3;
+    f1=f2=1;
+    cout<<"Enter a number:"<<endl;
+    cin>>a;
+    cout<<"The fibonacci series of "<<a<<" numbers is:";
+    cout<<f1<<" "<<f2;
+    for (j=3;j<=a;j++)
+    {
+        f3=f1+f2;
+        cout<<" "<<f3;
+        f1=f2;
+        f2=f3;
+    }
+    return 0;
+}
