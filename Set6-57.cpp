d68c039f18321fbfe8c1338fab65e298fb1d6fe2
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-14 21:55:31 +0530

    Create Set6-57.cpp

---
+++ Set6-57.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a,b,temp;;
+    cout<<"Enter 2 Num:";
+    cin>>a>>b;
+    temp=a;
+    a=b;
+    b=temp;
+    cout<<a<<" "<<b;
+    return 0;
+}
