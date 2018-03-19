43b0e49ab641a200c43ca6366b9f7bc110d67752
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-19 17:56:08 +0530

    Create Set7-68.cpp

---
+++ Set7/Set7-68.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int n;
+    cout<<"Enter a Num:";
+    cin>>n;
+    if(n%7==0)
+    cout<<"yes";
+    else
+    cout<<"no";
+    return 0;
+}
