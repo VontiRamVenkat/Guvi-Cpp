38571d51c2fe33ff9d57236ee0958c0c99232a2d
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-19 17:52:56 +0530

    Create Set7-67.cpp

---
+++ Set7/Set7-67.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+int main() 
+{
+	int n,i;
+	cout<<"Enter a number:";
+	cin>>n;
+	for(i=10;i<1000;i+=10)
+	{
+	    if(n<i)
+	    {
+	        cout<<i;
+	        break;
+	    }
+	}
+}
