f8e19297e4ed125dda8ec869cb26961d1349fe58
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-19 16:56:52 +0530

    Create Set7-61.cpp

---
+++ Set7/Set7-61.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    string s;
+    int i,k;
+    cout<<"Enter The String:";
+    getline(cin,s);
+    cout<<"Enter num to print the first K characters:";
+    cin>>k;
+    for(i=0;i<k;i++)
+    {
+    cout<<s[i];
+    }    
+    }
