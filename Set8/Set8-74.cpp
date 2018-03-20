184ce00c3841963d28c289a4df74ec5ea0f0e442
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-20 17:25:31 +0530

    Create Set8-74.cpp

---
+++ Set8/Set8-74.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include<cmath>
+using namespace std;
+int main()
+{
+    float a,res;
+    int i;
+    cout<<"Enter a decimal(only 1 decimal place) number:";
+    cin>>a;
+    res=round(a);
+    cout<<res;
+    return 0;
+}
