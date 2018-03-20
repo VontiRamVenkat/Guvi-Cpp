f51b8acbb30445c23f44f43b9f441ccd8e82c875
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-20 17:44:11 +0530

    Update Set9-82.cpp

--- Set9/Set9-82.cpp
+++ Set9/Set9-82.cpp
@@ -3,11 +3,11 @@
 using namespace std;
 int main()
 {
-  float l,b;
+  float a,b;
   double area;
   cout<<"Enter length(L) and breadth(B):";
-  cin>>l>>b;
-  area=l*b;
+  cin>>a>>b;
+  area=a*b;
   cout<<area<<setprecision(5);
       return 0;
 }
