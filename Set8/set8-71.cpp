9ad032347d2402ee3f725793e28a86d03b7608bf
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-03-20 17:01:51 +0530

    Create set8-71.cpp

---
+++ Set8/set8-71.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include<string.h>
+using namespace std;
+int main()
+{
+    string s;
+    int i,set=0;
+    cout<<"Enter The Sentence:";
+    getline(cin,s);
+    for(i=0;i<s.length();i++)
+    if(s[i]!=s[s.length()-i-1])
+    set=1;
+    if(set)
+    cout<<"No";
+    else
+    cout<<"yes";
+    return 0;
+}
