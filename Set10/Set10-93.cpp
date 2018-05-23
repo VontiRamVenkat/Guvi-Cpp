4e24f918393b2710f53967f3686c0e6d7b288725
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-05-23 09:28:58 +0530

    Create Set10-93.cpp

---
+++ Set10/Set10-93.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+     int a,init=273.15,sum;/*273.158*/
+     cout<<"Enter the Temperature in Celsius:";
+     cin>>a;
+     sum=a+init;
+     cout<<"Coverted and Displaying in Kelvin:"<<sum;
+     return 0;
+}
