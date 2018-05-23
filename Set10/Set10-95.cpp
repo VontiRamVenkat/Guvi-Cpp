31d7b80d9a5e911b23e5fc379fe68d0d506f4817
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-05-23 09:50:40 +0530

    Create Set10-95.cpp

---
+++ Set10/Set10-95.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+   int P,N,R,sum;
+    cout<<"To find Simple Interest\n";
+   cout<<"Enter P:";
+   cin>>P;
+    cout<<"Enter T:";
+   cin>>N;
+    cout<<"Enter R:";
+   cin>>R;
+   sum=P*N*R/100;
+   cout<<sum;
+	 return 0;
+  
+}
