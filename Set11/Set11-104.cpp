560b9738600699901aa58b612e830bb6ed06a425
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-05-23 14:01:58 +0530

    Create Set11-104.cpp

---
+++ Set11/Set11-104.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+  int n,m,res;
+  cout<<"Enter n:";
+  cin>>n;
+  cout<<"Enter m:";
+  cin>>m;
+  res=pow(n,m);
+  cout<<res;
+  return 0;
+}
