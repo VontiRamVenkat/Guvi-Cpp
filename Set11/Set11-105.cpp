165a27bdc6df4da34d9d3ec468b1253c6f971e26
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-05-23 16:04:55 +0530

    Create Set11-105.cpp

---
+++ Set11/Set11-105.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+#include <cmath>
+using namespace std;
+int main(){
+    int i,j,k;
+    cin>>i;
+    j = i + 64;
+    k = i + 96;
+    char ch,ch1;
+    ch = j;
+    ch1 = k;
+    cout<<ch<<" "<<ch1;
+  return 0;
+}
