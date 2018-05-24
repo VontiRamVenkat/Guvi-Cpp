d3301cf3c7344a21e3ca8659a91583ef2c72b2cf
VontiRamVenkat <32037880+VontiRamVenkat@users.noreply.github.com>
2018-05-24 09:50:16 +0530

    Create Set11-109.cpp

---
+++ Set11/Set11-109.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+#include<math.h>
+using namespace std;
+int main()
+{
+    int a,i,res;
+    cin>>a;
+    int n[a];
+     for(i=0;i<a;i++)
+    {
+     res=i<=a;
+    }
+     cout<<res;
+    return 0;
+}
